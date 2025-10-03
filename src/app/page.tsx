98f8ae31b88d2a2fd45883467e33f7ebe9a143f7
sth3r <vrodrigues.esther@Gmail.com>
2025-10-03 13:15:51 -0300

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import LunarCalendar from '@/components/LunarCalendar';
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center  from-indigo-950 via-purple-900 to-black">
+      {/* <meta name="google-adsense-account" content="ca-pub-3729829871518422"></meta> */}
       <div className="w-full max-w-3xl">
         <LunarCalendar />
       </div>
